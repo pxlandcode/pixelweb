1c8959a730422d4f21c95b8e328f8a256dc6b487
PierreElmen <pierre.elmen@gmail.com>
2025-10-25 23:55:46 +0200

    scrollgallery

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -3,3 +3,4 @@ export * from './rolling-text';
 
 export { default as Cardstack } from './Cardstack.svelte';
 export { default as CardstackItem } from './CardstackItem.svelte';
+export { default as ScrollGallery } from './ScrollGallery.svelte';
